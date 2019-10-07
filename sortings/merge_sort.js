07c4abadc2df1f04450dcd6f0dc673a12234535b
Maxim <petrloje@gmail.com>
2019-10-07 18:01:01 +0300

    Update сортировки слиянием. Заменён алгоритм. Массив не разбивается на два, вместо этого происходит разбота с тем же массивом посердством индексов и буффера.

--- sortings/merge_sort.js
+++ sortings/merge_sort.js
@@ -3,37 +3,42 @@
 /**
  * Функция конкатенирует два массива в один и возвращает его
 **/
-function concat(l, r) {
-  var i = 0,
-      j = 0,
-      a = [];
-  while(a.length !== (l.length + r.length)) {
-    if (l[i] == undefined || r[j] == undefined) {
-      a.push(l[i++] || r[j++]);
-      continue;
-    }
-    if (l[i] < r[j]) a.push(l[i++]);
-    else a.push(r[j++]);
+function merge(arr, l, m, r) {
+  const buf = new Array(r - l + 1);
+  var i = 0, h = l, j = m, k;
+  while((h <= m - 1) && j <= r) {
+    if (arr[h] <= arr[j]) buf[i] = arr[h++];
+    else buf[i] = arr[j++];
+    i++;
   }
-  return a;
+  // Добавление оставшихся элементов с левой части
+  while(h < m) buf[i++] = arr[h++];
+  // или справой
+  while(j <= r) buf[i++] = arr[j++];
+
+  for(i = 0; l <= r; l++, i++) {
+    arr[l] = buf[i];
+  }
+  return arr;
 }
 
 /**
  * Функция сорртировки разбиваем массив до базового случая
  * массива с одним элементом. Далее последовательно
- * сравнивает элементы массивов и при необходимости ставиит
+ * сравнивает элементы массивов и при необходимости ставить
  * их в соответствии с возрастающей последовательностью.
  */
 function merge_sort(arr) {
-  var middle = Math.round(arr.length / 2),
-        left, right, maxlen;
-  // Разделяем массив до базового случая
-  if (arr.length > 1) {
-    right = merge_sort(arr.slice(0, middle));
-    left = merge_sort(arr.slice(middle));
-  } else return arr; // возвращаем базовый случай
-
-  // Возвращаемый массив
-  const result = concat(left, right);
-  return result;
+  function mergeSort(arr, start, end) {
+    var middle,
+        diff = end - start;
+    if (diff !== 0) {
+      middle = Math.ceil(diff / 2) + start;
+      mergeSort(arr, start, middle - 1);
+      mergeSort(arr, middle, end);
+    } else return;
+    merge(arr, start, middle, end);
+  }
+  mergeSort(arr, 0, arr.length - 1);
+  return arr;
 }
